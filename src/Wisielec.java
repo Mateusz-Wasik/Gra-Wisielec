03cd343935844f1fedd599e1fd097ebc376846e2
Mateusz-Wasik <mateuszwasik85@gmail.com>
2024-03-06 22:23:46 +0100

    added checking if you win or loose

--- src/Wisielec.java
+++ src/Wisielec.java
@@ -8,10 +8,14 @@ public class Wisielec {
     List<String> words = List.of("komputer","monitor","klawiatura","mysz");
     String word;
     char[]userWord;
+
+    int lives=8;
+
+
     public void play() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("start gry");
+        System.out.println("Wisielec - gra polega na zgadywaniu kkolejnych liter w słowie. Kategoria-sprzęt komputerowy. ZACZYNAMY!");
 
         Random random = new Random();
         word = words.get(random.nextInt(words.size()));
@@ -26,7 +30,9 @@ public class Wisielec {
 
 
 
-        while (!gameEnded()) {
+
+
+        while (!gameEnded() && !outOfLives()) {
             System.out.println(userWord);
             System.out.println("podaj kolejną literę");
             char letter = scanner.nextLine().charAt(0);
@@ -34,26 +40,60 @@ public class Wisielec {
 
         }
 
+        if(gameEnded()){
+            System.out.println("BRAWO, odgadles cale haslo. Poprawne haslo to:"+word);
+        }
+        else {
+            System.out.println("Niestety nie masz wiecej prób. Poprawna odpowiedz to: "+word);
+        }
+
+
+
 
         scanner.close();
     }
 
 
 
+
+
+
+
     private void checkLetter(char letter){
+        boolean foundLetter = false;
+
         for(int i=0; i<word.length(); i++){
             if(word.charAt(i) == letter){
                 userWord[i]=letter;
+                foundLetter=true;
             }
+
+
+            }
+
+        if(!foundLetter){
+            System.out.println("Niestety nie ma tekiej litery w hasle. Pozostało ci jeszcze "+(lives-1)+" prób.");
+            lives--;
+
+        }
         }
 
+
+    private boolean gameEnded()
+    {
+        return word.equals(String.valueOf(userWord));
+
+    }
+
+    private boolean outOfLives(){
+        return lives == 0;
+    }
+
     }
 
 
 
 
-    private boolean gameEnded() {
-        return false;
-    }
 
-}
+
+
